a6208ece75867aeda687bb44b22817faff7f3acd
Mantas Antanaitis <antanaitis.web@gmail.com>
2021-07-15 11:27:58 +0300

    debug

--- src/api/sign-in.js
+++ src/api/sign-in.js
@@ -5,22 +5,22 @@ const handler = async (req, res) => {
     res.status(404).json({
       message: `${req.method} /sing-in not found`
     })
-    return;
+    return
   }
 
   if (!req.body.email && !req.body.password) {
-    res.status(500).json({message: "Missing required fields email and password"});
+    res.status(500).json({ message: "Missing required fields email and password" })
   }
 
   try {
-    const {data} = await axios.post(`${process.env.GATSBY_NETLIFY_IDENTITY_URL}/token?grant_type=password&username=${req.body.email}&password=${req.body.password}`, {
+    const { data } = await axios.post(`${process.env.GATSBY_NETLIFY_IDENTITY_URL}/token?grant_type=password&username=${req.body.email}&password=${req.body.password}`, {
       email: req.body.email,
       password: req.body.password
     })
     res.status(200).json(data)
-  }
-  catch (e) {
-    res.status(500).json({message: e.response ? e.response.data.error_description : "Unexpected error occurred"});
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: e.response ? e.response.data.error_description : "Unexpected error occurred" })
   }
 }
 
